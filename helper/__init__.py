d4faa3b0a53d02ab289f05aa2604a39e8af2a80d
ccaballeroh <cdch10@gmail.com>
2021-02-24 23:05:41 -0600

    change name on drive folder

--- helper/__init__.py
+++ helper/__init__.py
@@ -10,7 +10,7 @@ from pathlib import Path
 IN_COLAB = "google.colab" in sys.modules
 
 if IN_COLAB:
-    ROOT = Path(r"./drive/My Drive/MCPR-2021/")  # Root folder in colab
+    ROOT = Path(r"./drive/My Drive/translator-attribution/")  # Root folder in colab
     print("In colab!")
 else:
     ROOT = Path(r".")  # Root folder if running locally
